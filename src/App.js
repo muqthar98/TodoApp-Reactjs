b0af7d01bf7cdd1c0eeacce158c94c143aa640f5
muqthar98 <53577194+muqthar98@users.noreply.github.com>
2021-03-14 11:31:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import {Button,Input} from 'reactstrap';
+import { Button,InputLabel,Input,FormControl } from '@material-ui/core';
 
 export default function App(){
+
+  const[todos,setTodos] = useState("");
+  const[input,setInput] = useState("");
+
+  const addTodo = {Event}{
+    setTodos([...todos,input]);
+  }
+
+  }
   return(
   <div>
     <div className="header">
       Todo App
     </div>
-	<div className="form">
-	<Input/>
+	<FormControl>
+  <InputLabel>Todo</InputLabel>
+  <Input value={input} onChange={event => setInput[event.target.value]}/>
 	<Button className="btn-md btn-block btn-light Add">Add Todo</Button>
 	<Button className="btn-md btn-block btn-dark Delete">Delete Todo</Button>
+  </FormControl>
+  <div>
+    <ul>
+      {todos.map(todo=>(
+       <li>{todo}</li>
+      ))}
+      
+    </ul>
+  </div>
 	</div>
-	</div>
+ 
   )
 }
